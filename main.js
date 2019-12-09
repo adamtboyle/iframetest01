f3f2915ae028e97223f976239e94a309817c44f9
adamtboyle <adamtboyle@gmail.com>
2019-12-09 11:15:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,8 @@ function onClick(e) {
 }
 
 function postClickMessage(value) {
-   window.postMessage('Hello from postClickMessage Function');
+   //window.postMessage('Hello from postClickMessage Function');
+    window.parent.postMessage('Hello Post Function', '*');
 }
 
 function receiveMessage(e) {
